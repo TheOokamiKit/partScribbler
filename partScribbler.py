0575f736f8b5593c481ee084d17c34d356adc953
TheOokamiKit <tyranime@gmail.com>
2025-05-20 22:47:32 -0500

    added custom calculations to weapons

--- partScribbler.py
+++ partScribbler.py
@@ -81,6 +81,8 @@ def partScribbler():
     # sort directory of parts into catigories and calculate additional stats evaluations that are commonly used in the spreadsheet
     def sort_parts(list_of_parts):
         for part in list_of_parts:
+
+            # Cockpit part calculations commonly used in the spreadsheet
             if part["parttype"] == 'Cockpit':
                 part["HP_Weight"] = part["hitpoints"] / part["weight"]
                 part["NetEnergy"] = part["energygen"] - part["energyuse"]
@@ -88,22 +90,74 @@ def partScribbler():
                 part["HeatCapacity"] = part["heatThreshold"] - part["heatgen"] + part["cooling"]
                 part["TotalWeapons"] = part["weapongroups"] * part["weaponspergroup"]
                 Cockpit.append(part)
+
+            # Mobility part calculate commonly used in the spreadsheet
             elif part["parttype"] == 'Mobility':
                 part["HeatCapacity"] = part["heatThreshold"] - part["heatgen"] + part["cooling"]
                 part["MaxSpeed"] = part["forwardspeed"] * 4.1738
                 part["NetEnergy"] = part["energygen"] - part["energyuse"]
                 Mobility.append(part)
+
+            # Weapon part calculations for custom stats and renaming stats for ease of readablity.
             elif part["parttype"] == 'Weapon':
-                #part.["DP_S"] - part["damage"]
+                # current workaround for weapon damage not being reported by type in input data. Handles known exceptions that do both types first.
+                if part["partname"] == "Screamer Rocket" or part["partname"] == "Gustaf - X2":
+                    part["BlastDamage"] = part["damage"]
+                    part["KineticDamage"] = part["damage"]
+                elif part.setdefault("blastArea",0) > 0:
+                    part["BlastDamage"] = part["damage"]
+                    part["KineticDamage"] = 0
+                else:
+                    part["BlastDamage"] = 0
+                    part["KineticDamage"] = part["damage"]
+
+                part["NetEnergy"] = part["energygen"] - part["energyuse"]
+                part["Cooling_Weight"] = part["cooling"] / part["weight"]
+                part["HP_Weight"] = part["hitpoints"] / part["weight"]
+                part["DamageCapacity"] = (part["BlastDamage"] + part["KineticDamage"]) * part["shotsperfire"] * part["bulletsperclip"]
+                part["DC_Weight"] = part["DamageCapacity"] / part["weight"]
+                part["RateOfFire_BPM"] = 60 / (part["firerate"] + (part.setdefault("firedelay",0.1) * (part["burstfire"] - 1) ) )
+                part["ammo"] = part["bulletsperclip"]
+                part["TimeTilEmptyM"] = part["ammo"]  / part["burstfire"] / part["RateOfFire_BPM"]
+                if part["spreadX"] > part["spreadY"]:
+                    part["MaxSpread"] = part["spreadX"]
+                else:
+                    part["MaxSpread"] = part["spreadY"]
+
+                #todo change initialspeed to MaximumSpeed once serge is able to pull stat from game files. Currently rockets look bad as they accelerate mid flight. Working with what is available currently
+                if part["initialspeed"] > 1:
+                    part["TimeToMaxRange"] = part["range"] / part["initialspeed"]
+                else:
+                    part["TimeToMaxRange"] = 0
+
+                part["DP_S"] = ( (part["BlastDamage"] + part["KineticDamage"]) * part["RateOfFire_BPM"] * part["burstfire"] * part["shotsperfire"]) / 60
+                part["DP_S_Weight"] = part["DP_S"] / part["weight"]
+                part["%_base_HP_M"] = ( ( (part["BlastDamage"] * 2) + part["KineticDamage"]) * part["RateOfFire_BPM"] * part["burstfire"] * part["shotsperfire"]) / 21000
+                part["%_base_HP_M_W"] = part["%_base_HP_M"] / part["weight"]
+                part["HeatP_S"] = part["impactHeat"] * part["RateOfFire_BPM"] * part["burstfire"] * part["shotsperfire"] / 60
+                part["BurstDamage"] = (part["BlastDamage"] + part["KineticDamage"]) * part["burstfire"] * part["shotsperfire"]
+                part["BurstDamage_W"] = part["BurstDamage"] / part["weight"]
+                part["BurstHeat"] = part["impactHeat"] * part["burstfire"] * part["shotsperfire"]
+                part["BurstHeat_W"] = part["BurstHeat"] / part["weight"]
+                #todo 200m accuracy math
+                #todo 500m accuracy math
+                #todo max range accuracy math
+
+
                 Weapon.append(part)
+
             elif part["parttype"] == 'Armor':
                 Armor.append(part)
+
             elif part["parttype"] == 'Generator':
                 Generator.append(part)
+
             elif part["parttype"] == 'Aux':
                 Aux.append(part)
+
             elif part["parttype"] == 'Spacer':
                 Spacer.append(part)
+
             else:
                 Other.append(part)
 
@@ -121,10 +175,10 @@ def partScribbler():
         ]
 
     fields_Weapon = [
-        "partname","subType","ID","hitpoints","armor","energygen","energyuse","heatgen","cooling","heatThreshold","weight",
-        "range","lifetime","firerate","burstfirerate","force","damage","bulletsperclip","shotsperfire","burstfire","reloadtime",
-        "initialspeed","spreadX","spreadY","kickforce","impactHeat","blastArea",
-        "cost","manufacturer","description","unlock"
+        "partname","subType","DP_S","DP_S_Weight","%_base_HP_M","%_base_HP_M_W","KineticDamage","BlastDamage","BurstDamage","BurstDamage_W","BurstHeat","BurstHeat_W","force",
+        "initialspeed","TimeToMaxRange","spreadX","spreadY","MaxSpread","kickforce","range","ammo","RateOfFire_BPM","firerate","TimeTilEmptyM","DamageCapacity","DC_Weight",
+        "hitpoints","HP_Weight","armor","weight","NetEnergy","heatgen","cooling","Cooling_Weight","heatThreshold","burstfirerate","shotsperfire","burstfire","reloadtime",
+        "impactHeat","blastArea","lifetime","cost","manufacturer","description","unlock","ID"
         ]
 
     fields_Armor = [
